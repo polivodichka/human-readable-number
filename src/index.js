30b9753db4934a6da3d1411bbf0cd76e1035da26
Anastasia Polivoda <polivoda.anastasiia@gmail.com>
2022-01-08 22:48:39 +0500

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
+    let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    let tens = [, , 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+ 
   
+    return number < 20? 
+        units[number]:
+    number < 100? 
+        tens[Math.trunc(number/10)] + (number % 10 != 0 ? ' ' + units[number % 10] : ''): 
+    number<1000?
+        units[Math.trunc(number/100)] + ' hundred' + (number%100!=0?' ' + toReadable(number%100):''): 
+    'Sorry, can\'t read it :(';
 }
